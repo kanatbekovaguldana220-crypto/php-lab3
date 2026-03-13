654346339c987130c07c30d25ee1d29fa4d36fbf
kanatbekovaguldana220-crypto <kanatbekovaguldana220@gmail.com>
2026-03-13 21:28:58 +0600

    Add files via upload

---
+++ Guldana.ist/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <style>
+        table { 
+            border-collapse: collapse; 
+            border-top: 10px solid yellow;
+            border-left: 10px solid yellow;
+        }
+        td { 
+            width: 30px; 
+            height: 30px; 
+            border: 1px solid #ddd; 
+        }
+        .black { background-color: black; }
+        .white { background-color: white; }
+    </style>
+</head>
+<body>
+
+<?php
+$size = 7; 
+$board = [];
+
+
+for ($row = 0; $row < $size; $row++) {
+    for ($col = 0; $col < $size; $col++) {
+        $board[$row][$col] = ($row + $col) % 2 == 0 ? 0 : 1;
+    }
+}
+
+
+echo "<table>";
+foreach ($board as $rowArray) {
+    echo "<tr>";
+    foreach ($rowArray as $cell) {
+        $colorClass = ($cell == 1) ? 'black' : 'white';
+        echo "<td class='$colorClass'></td>";
+    }
+    echo "</tr>";
+}
+echo "</table>";
+?>
+
+</body>
+</html>
